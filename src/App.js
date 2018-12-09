10c54215eede37056671a30d88b16cebcae59351
j-yama <j-yama@example.com>
2018-12-09 16:44:54 +0900

    noW

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ReactiveBase, DataSearch, MultiList, SingleRange, RangeSlider, ResultCard, CategorySearch } from '@appbaseio/reactivesearch';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="main-container">
+        <ReactiveBase
+          app="steam-search"
+          url="http://localhost:9200"
+          theme={
+            {
+              typography: {
+                fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif',
+                fontSize: '16px',
+              },
+
+              colors: {
+                textColor: '#fff',
+                backgroundColor: '#212121',
+                primaryTextColor: '#fff',
+                primaryColor: '#2196F3',
+                titleColor: '#fff',
+                alertColor: '#d9534f',
+                borderColor: '#666',
+              }
+            }
+        }
+      >
+          <CategorySearch
+          componentId="searchbox"
+          dataField="QueryName"
+          categoryField="ResponseName"
+          placeholder="ストアを検索する"
+        />
+        <ResultCard
+          componentId="results"
+          dataField="name"
+          size={10}
+          pagination={true}
+          react={{
+            and: ["searchbox", "ratingsfilter"]
+          }}
+          onData={(res) => {
+            return {
+              image: res.HeaderImage,
+              title: res.QueryName,
+              description: `Release Date: ${res.ReleaseDate}<br/>Price: $${res.PriceInitial}`
+            }
+          }}
+        />
+        </ReactiveBase>
+      </div >
     );
   }
 }
-
 export default App;
